903c3f9796b24067504ecfd70396ce8a3c9d490c
Zheng Yijia <zhengyijia142@qq.com>
2018-07-31 11:34:37 +0800

    Add CORS support.

---
+++ src/main/java/com/siat/config/CORSConfig.java
@@ -0,0 +1,19 @@
+package com.siat.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// 添加跨域支持
+@Configuration
+public class CORSConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOrigins("*")
+                .allowedHeaders("*");
+    }
+
+}
